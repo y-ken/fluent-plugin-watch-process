3a1cf7bb85e9281b32e200ab19f05f10d90af9ee
daipom <reangoapyththeorem@gmail.com>
2021-05-14 18:55:05 +0900

    Support Windows
    TODO: `types` setting is not yet reflected.

--- lib/fluent/plugin/in_watch_process.rb
+++ lib/fluent/plugin/in_watch_process.rb
@@ -1,4 +1,5 @@
 require 'time'
+require 'csv'
 require "fluent/plugin/input"
 require 'fluent/mixin/rewrite_tag_name'
 require 'fluent/mixin/type_converter'
@@ -8,8 +9,6 @@ module Fluent::Plugin
     Fluent::Plugin.register_input('watch_process', self)
 
     helpers :timer
-
-    DEFAULT_TYPES = "pid:integer,parent_pid:integer,cpu_percent:float,memory_percent:float,mem_rss:integer,mem_size:integer"
 
     config_param :tag, :string
     config_param :command, :string, :default => nil
@@ -21,7 +20,6 @@ module Fluent::Plugin
     include Fluent::HandleTagNameMixin
     include Fluent::Mixin::RewriteTagName
     include Fluent::Mixin::TypeConverter
-    config_set_default :types, DEFAULT_TYPES
 
     def initialize
       super
@@ -60,6 +58,8 @@ module Fluent::Plugin
           Linux.new(keys, command)
         elsif OS.mac?
           Mac.new(keys, command)
+        elsif OS.windows?
+          Windows.new(keys, command)
         else
           raise NotImplementedError, "This OS type is not supported."
         end
@@ -103,6 +103,7 @@ module Fluent::Plugin
 
       class BaseUnixLike < Base
         DEFAULT_KEYS = %w(start_time user pid parent_pid cpu_time cpu_percent memory_percent mem_rss mem_size state proc_name command)
+        DEFAULT_TYPES = "pid:integer,parent_pid:integer,cpu_percent:float,memory_percent:float,mem_rss:integer,mem_size:integer"
 
         def initialize(keys, command)
           super(keys, command)
@@ -141,6 +142,97 @@ module Fluent::Plugin
         def initialize(keys, command)
           super(keys, command)
           @command = @command || DEFAULT_COMMAND
+        end
+      end
+
+      class Windows < Base
+        # Values are from the "System.Diagnostics.Process" object properties that can be taken by the "Get-Process" command.
+        # You can check the all properties by the "(Get-Process)[0] | Get-Member" command.
+        DEFAULT_PARAMS = {
+          "start_time" => "StartTime",
+          "user" => "UserName",
+          "sid" => "SessionId",
+          "pid" => "Id",
+          "cpu_second" => "CPU",
+          "working_set" => "WorkingSet",
+          "virtual_memory_size" => "VirtualMemorySize",
+          "handles" => "HandleCount",
+          "proc_name" => "ProcessName",
+        }
+        DEFAULT_TYPES = "sid:integer,pid:integer,cpu_second:float,working_set:integer,virtual_memory_size:integer,handles:integer"
+
+        def initialize(keys, command)
+          super(keys, command)
+          @keys = @keys || DEFAULT_PARAMS.keys
+          @command = @command || default_command
+        end
+
+        def parse_line(line)
+          values = line.chomp.strip.parse_csv.map { |e| e ? e : "" }
+          Hash[@keys.zip(values)]
+        end
+
+        def default_command
+          command = [
+            command_ps,
+            pipe_filtering_normal_ps,
+            pipe_select_columns,
+            pipe_fixing_locale,
+            pipe_formatting_output,
+          ].join
+          "powershell -command \"#{command}\""
+        end
+
+        private
+
+        def command_ps
+          if @keys.include?("user")
+            # The "IncludeUserName" option is needed to get the username, but this option requires Administrator privilege.
+            "Get-Process -IncludeUserName"
+          else
+            "Get-Process"
+          end
+        end
+
+        def pipe_filtering_normal_ps
+          # There are some special processes that don't have some properties, such as the "Idle" process.
+          # Normally, these are specific to the system and are not important, so exclude them.
+          # Note: The same situation can occur in some processes if there are no Administrator privilege.
+          " | ?{$_.StartTime -ne $NULL -and $_.CPU -ne $NULL}"
+        end
+
+        def pipe_select_columns
+          columns = DEFAULT_PARAMS.keys.map { |key|
+            next unless @keys.include?(key)
+            DEFAULT_PARAMS[key]
+          }.compact
+
+          if columns.nil? || columns.empty?
+            raise "The 'keys' parameter is not specified correctly. [keys: #{@keys}]"
+          end
+
+          " | Select-Object -Property #{columns.join(',')}"
+        end
+
+        def pipe_fixing_locale(format: "ddd MMM dd HH:mm:ss yyyy", locale: "en-US")
+          # In Windows, setting the "$env:Lang" environment variable is not effective in changing the format of the output.
+          # You can use "Datetime.ToString" method to change format of datetime values in for-each pipe.
+          # Note: About "DateTime.ToString" method: https://docs.microsoft.com/en-us/dotnet/api/system.datetime.tostring
+          # Note: About "Custom date and time format strings": https://docs.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
+          " | %{
+            $_.StartTime = $_.StartTime.ToString(
+              '#{format}',
+              [Globalization.CultureInfo]::GetCultureInfo('#{locale}').DateTimeFormat
+            ); 
+            return $_;
+          }"
+        end
+
+        def pipe_formatting_output
+          # In the "ConvertTo-Csv" command, there are 2 lines of type info and header info at the beginning in the outputs.
+          # By the "NoTypeInformation" option, the line of type info is excluded.
+          # This enables you to skip the just first line for parsing, like linux or mac.
+          " | ConvertTo-Csv -NoTypeInformation"
         end
       end
 
