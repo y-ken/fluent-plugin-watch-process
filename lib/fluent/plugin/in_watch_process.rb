a82b3fdd2c5c6bdcb2622fe11aed7c948e5c28a5
daipom <reangoapyththeorem@gmail.com>
2021-05-17 19:57:49 +0900

    Fix to use Fluent module functions to identify OS type

--- lib/fluent/plugin/in_watch_process.rb
+++ lib/fluent/plugin/in_watch_process.rb
@@ -89,32 +89,17 @@ module Fluent::Plugin
     end
 
     def get_ps_command
-      if OS.linux?
-        "LANG=en_US.UTF-8 && ps -ewwo lstart,user:20,pid,ppid,time,%cpu,%mem,rss,sz,s,comm,cmd"
-      elsif OS.mac?
+      if mac?
         "LANG=en_US.UTF-8 && ps -ewwo lstart,user,pid,ppid,time,%cpu,%mem,rss,vsz,state,comm,command"
       elsif Fluent.windows?
         @windows_watcher.command
+      else
+        "LANG=en_US.UTF-8 && ps -ewwo lstart,user:20,pid,ppid,time,%cpu,%mem,rss,sz,s,comm,cmd"
       end
     end
 
-    module OS
-      # ref. http://stackoverflow.com/questions/170956/how-can-i-find-which-operating-system-my-ruby-program-is-running-on
-      def OS.windows?
-        (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-      end
-
-      def OS.mac?
-       (/darwin/ =~ RUBY_PLATFORM) != nil
-      end
-
-      def OS.unix?
-        !OS.windows?
-      end
-
-      def OS.linux?
-        OS.unix? and not OS.mac?
-      end
+    def mac?
+      (/darwin/ =~ RUBY_PLATFORM) != nil
     end
 
     class WindowsWatcher
