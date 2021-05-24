0fcd97df35e7d90d9dd2953bb4ad056f7003deaf
daipom <reangoapyththeorem@gmail.com>
2021-05-24 14:12:00 +0900

    Refactor test

--- test/plugin/test_in_watch_process.rb
+++ test/plugin/test_in_watch_process.rb
@@ -26,7 +26,7 @@ class WatchProcessInputTest < Test::Unit::TestCase
   end
 
   def test_unixlike
-    return if Fluent.windows?
+    omit "Only for UNIX like." if Fluent.windows?
     whoami = `whoami`
     d = create_driver %[
       tag          input.watch_process
@@ -37,74 +37,76 @@ class WatchProcessInputTest < Test::Unit::TestCase
     assert(d.events.size > 1)
   end
 
-  def test_windows_default
-    return unless Fluent.windows?
-    d = create_driver %[
-      tag input.watch_process
-      interval 1s
-    ]
-    default_keys = Fluent::Plugin::WatchProcessInput::WindowsWatcher::DEFAULT_PARAMS.keys
+  sub_test_case "Windows" do
+    def test_windows_default
+      omit "Only for Windows." unless Fluent.windows?
+      d = create_driver %[
+        tag input.watch_process
+        interval 1s
+      ]
+      default_keys = Fluent::Plugin::WatchProcessInput::WindowsWatcher::DEFAULT_PARAMS.keys
 
-    d.run(expect_records: 1, timeout: 10);
+      d.run(expect_records: 1, timeout: 10);
 
-    assert d.events.size > 0
+      assert d.events.size > 0
 
-    tag, time, record = d.events[0]
+      tag, time, record = d.events[0]
 
-    assert_equal "input.watch_process", tag
-    assert time.is_a?(Fluent::EventTime)
-    assert_equal default_keys, record.keys
-  end
-
-  def test_windows_customized
-    return unless Fluent.windows?
-    custom_keys = ["handles", "pid", "proc_name"]
-    d = create_driver %[
-      tag input.watch_process
-      interval 1s
-      keys #{custom_keys.join(",")}
-      types pid:integer
-    ]
-
-    d.run(expect_records: 1, timeout: 10);
-
-    assert d.events.size > 0
-
-    tag, time, record = d.events[0]
-
-    assert_equal "input.watch_process", tag
-    assert time.is_a?(Fluent::EventTime)
-    assert_equal custom_keys, record.keys
-    assert record["handles"].is_a?(String)
-    assert record["pid"].is_a?(Integer)
-  end
-
-  def test_windows_lookup
-    return unless Fluent.windows?
-    d = create_driver %[
-      tag input.watch_process
-      interval 1s
-    ]
-    d.run(expect_records: 1, timeout: 10);
-
-    assert d.events.size > 0
-
-    tag, time, record = d.events[0]
-    lookup_user = record["user"]
-
-    d = create_driver %[
-      tag input.watch_process
-      interval 1s
-      lookup_user #{lookup_user}
-    ]
-    d.run(expect_records: 1, timeout: 10);
-
-    assert d.events.size > 0
-
-    other_user_records = d.events.reject do |tag, time, record|
-      lookup_user.include?(record["user"])
+      assert_equal "input.watch_process", tag
+      assert time.is_a?(Fluent::EventTime)
+      assert_equal default_keys, record.keys
     end
 
-    assert other_user_records.size == 0
+    def test_windows_customized
+      omit "Only for Windows." unless Fluent.windows?
+      custom_keys = ["handles", "pid", "proc_name"]
+      d = create_driver %[
+        tag input.watch_process
+        interval 1s
+        keys #{custom_keys.join(",")}
+        types pid:integer
+      ]
+
+      d.run(expect_records: 1, timeout: 10);
+
+      assert d.events.size > 0
+
+      tag, time, record = d.events[0]
+
+      assert_equal "input.watch_process", tag
+      assert time.is_a?(Fluent::EventTime)
+      assert_equal custom_keys, record.keys
+      assert record["handles"].is_a?(String)
+      assert record["pid"].is_a?(Integer)
+    end
+
+    def test_windows_lookup
+      omit "Only for Windows." unless Fluent.windows?
+      d = create_driver %[
+        tag input.watch_process
+        interval 1s
+      ]
+      d.run(expect_records: 1, timeout: 10);
+
+      assert d.events.size > 0
+
+      tag, time, record = d.events[0]
+      lookup_user = record["user"]
+
+      d = create_driver %[
+        tag input.watch_process
+        interval 1s
+        lookup_user #{lookup_user}
+      ]
+      d.run(expect_records: 1, timeout: 10);
+
+      assert d.events.size > 0
+
+      other_user_records = d.events.reject do |tag, time, record|
+        lookup_user.include?(record["user"])
+      end
+
+      assert other_user_records.size == 0
+    end
   end
 end
